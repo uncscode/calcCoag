6ce81d37cf560a19e39f659a26f6bad9920179dc
ngam <67342040+ngam@users.noreply.github.com>
2023-02-14 21:41:47 -0500

    add simple code to start

---
+++ code/dimLessCoag.m
@@ -0,0 +1,21 @@
+function kernel = calc_coag(diffusive_knudsen, columbic_potential_ratio, approx)
+    % Purpose: calculate the coagulation kernel
+    % Inputs:
+    %   diffusive_knudsen: diffusive knudsen number
+    %   columbic_potential_ratio: columbic potential ratio
+    %   approx: approximation to use (hardsphere, etc.)
+    % Outputs:
+    %   kernel: coagulation kernel
+
+    cpr_mod = (1e-16) .* (columbic_potential_ratio == 0);
+    cpr = cpr_mod + columbic_potential_ratio;
+
+    cekl = 1 + cpr;
+    cecl = 1 .* (cpr == 0) + (cpr ./ (1 - exp(-cpr))) .* (cpr ~= 0);
+
+    coag_cont_lim = 4 .* pi() .* diffusive_knudsen.^2;
+    coag_hs = hardsphere_approx(coag_cont_lim, diffusive_knudsen);
+
+
+    kernel = coag_hs;
+end
---
+++ code/hardsphereApprox.m
@@ -0,0 +1,21 @@
+function hs = hardsphere_approx(coag_cont_lim, diffusive_knudsen)
+    % Purpose: calculates the hard sphere coag approximation
+    % Inputs:
+    %   coag_cont_lim: the coagulation continuum limit
+    %   diffusive_knudsen: the diffusive knudsen number
+    % Outputs:
+    %   hs: the hardsphere coagulation approximation
+
+    hsa_consts = [25.836 11.211 3.502 7.211];
+
+    numerator = (coag_cont_lim) ...
+                + (hsa_consts(1) .* diffusive_knudsen.^3) ...
+                + ((8.*pi()).^(1/2) .* hsa_consts(2) .* diffusive_knudsen.^4);
+
+    denominator = (1) ...
+                + (hsa_consts(3) .* diffusive_knudsen) ...
+                + (hsa_consts(4) .* diffusive_knudsen.^2) ...
+                + (hsa_consts(2) .* diffusive_knudsen.^3);
+
+    hs = numerator ./ denominator;
+end
